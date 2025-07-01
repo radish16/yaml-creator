e4228aad561f7915080b014376885865ef83c15e
radish16 <150293215+radish16@users.noreply.github.com>
2025-07-01 23:06:05 +0900

    外部からアクセスできるよう修正

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import yaml
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def index():
     return render_template("index.html", yaml_output=yaml_output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Renderが指定するポートを取得
+    app.run(debug=False, host="0.0.0.0", port=port)  # 外部アクセス許可
